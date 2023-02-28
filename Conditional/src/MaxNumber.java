f2279f1904016b10631168e9e4a04757e2d1ec9f
Fuad Khan <107122588+Fuad-Khan@users.noreply.github.com>
2023-02-28 23:59:31 +0600

    Update MaxNumber.java

--- Conditional/src/MaxNumber.java
+++ Conditional/src/MaxNumber.java
@@ -4,7 +4,16 @@ public class MaxNumber {
         int num2 = 20;
         int num3 = 30;
 
-        int max = Math.max(num1, Math.max(num2, num3));
+        int max = num1;
+
+        if (num2 > max) {
+            max = num2;
+        }
+
+        if (num3 > max) {
+            max = num3;
+        }
+
         System.out.println("The maximum value is: " + max);
     }
 }
